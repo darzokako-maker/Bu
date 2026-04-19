06280b1665f0ca4415b4b00eaa1d4c99ef97d231
darzokako-maker <darzokako@gmail.com>
2026-04-19 13:43:30 +0300

    Implement Update function in AutoDumper namespace
    Add Update function to AutoDumper namespace for future implementation.

---
+++ src/loader/dumper.hpp
@@ -0,0 +1,9 @@
+namespace AutoDumper {
+    bool Update() {
+        // Burada client.dll dosyasını tarayıp 
+        // dwLocalPlayer, dwEntityList gibi offsetleri 
+        // hafızadan çeken kodlar yer alacak.
+        return true; 
+    }
+}
+
